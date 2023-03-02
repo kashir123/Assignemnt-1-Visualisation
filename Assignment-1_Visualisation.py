2afbaa1832280345e453b42e122ac5da84d3e2bd
kashir123 <kashir.waseem116@gmail.com>
2023-03-02 00:37:49 +0000

    all three graph are done without some title's and proper coding standard

--- Assignment-1_Visualisation.py
+++ Assignment-1_Visualisation.py
@@ -30,12 +30,56 @@ def draw_line_plot_graph(dataset):
     
     for year and consumption
     """
-    df_actual_dataset = dataset.groupby("YEAR")['EMISSIONS'].sum().reset_index()
-
-    plt.plot(df_actual_dataset['YEAR'], df_actual_dataset['EMISSIONS'])
+    df_actual_dataset = (dataset
+                         .groupby("YEAR")['FUEL CONSUMPTION']
+                         .sum()
+                         .reset_index()
+                         )
+    
+    plt.plot(df_actual_dataset['YEAR']
+             , df_actual_dataset['FUEL CONSUMPTION'])
     plt.xticks(np.arange(2000,2022,2))
     plt.show()
-    return 0
 
+"""
+Function for the pie plot graph
+"""
+def draw_pie_plot_graph(dataset):
+    """
+    Pie plot graph 
+    
+    for year and consumption
+    """
+    df_actual_dataset = (dataset
+                         .groupby("YEAR")['FUEL CONSUMPTION']
+                         .sum().sort_values(ascending = False)
+                         .reset_index())
+    plt.pie(df_actual_dataset['FUEL CONSUMPTION'][:5]
+          , labels = df_actual_dataset['YEAR'][:5]
+          , autopct = "%1.2f%%")
+    plt.show()
+    
+"""
+Function for the Bar plot graph
+"""
+def draw_bar_plot_graph(dataset):
+    """
+    Bar plot graph 
+    
+    for year and consumption
+    """
+    df_actual_dataset = (dataset
+                         .groupby("MAKE")['FUEL CONSUMPTION']
+                         .sum().sort_values(ascending = False)
+                         .reset_index())
+    fig = plt.figure()
+    ax = fig.add_axes([0,0,1,1])
+    ax.bar(df_actual_dataset['MAKE'][:5]
+            ,df_actual_dataset['FUEL CONSUMPTION'][:5])
+    plt.show()
+    
+    
 df = read_dataSet('Fuel_Consumption_2000-2022.csv')
 draw_line_plot_graph(df)
+draw_pie_plot_graph(df)
+draw_bar_plot_graph(df)
