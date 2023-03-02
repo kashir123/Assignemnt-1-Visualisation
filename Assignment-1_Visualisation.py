992e031ddf55b0f3e5bde3ce5be2c7fc157b48bf
kashir123 <kashir.waseem116@gmail.com>
2023-03-02 17:58:34 +0000

    final version 1.0

--- Assignment-1_Visualisation.py
+++ Assignment-1_Visualisation.py
@@ -2,84 +2,132 @@
 
 """
 
-@author: Kashir
+@author: Kashir Waseem Assignment-1 Data Visualisation
+Student id : 22031825
+Course : Applied Data Science 1 
 """
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-"""
-Function to read the dataset
-"""
+
 def read_dataSet(path):
     """
-    Read dataset
+    Dataset Reading Function
     
-    using the path from local
-    
+    I use pandas and get the data
+    from the local directory
     """
     df = pd.read_csv(path)
     return df
 
-"""
-Function for the line plot graph
-"""
-def draw_line_plot_graph(dataset):
+
+def draw_line_plot_graph(x,y):
     """
     Line plot graph 
     
-    for year and consumption
-    """
-    df_actual_dataset = (dataset
-                         .groupby("YEAR")['FUEL CONSUMPTION']
-                         .sum()
-                         .reset_index()
-                         )
+    I am using the column Year and fuel Consumption
+    to plot the line graph this will conslude that 
+    how the consumption of fuel goes over the years
     
-    plt.plot(df_actual_dataset['YEAR']
-             , df_actual_dataset['FUEL CONSUMPTION'])
+    The function accept two parameter x and y
+    x contain the value which will draw on 
+    x-axis and y will contain the value will
+    draw on y axis. On X axis we have a Years
+    and on Y axis we have of all the fuel consumption
+    over the year
+    """
+    #set the size of the figure
+    plt.figure(figsize=(15,10))
+    #labels
+    plt.title("Fuel consumption over the year")
+    plt.xlabel("YEAR")
+    plt.ylabel("Fuel Consumption")
+    # plot
+    plt.plot(x, y)
+  
+    # setting xticks
     plt.xticks(np.arange(2000,2022,2))
+    # show plot
     plt.show()
 
-"""
-Function for the pie plot graph
-"""
-def draw_pie_plot_graph(dataset):
+
+def draw_pie_plot_graph(x, y):
     """
     Pie plot graph 
     
-    for year and consumption
+    The function accept 2 parameter one for the value
+    associated with each category and the second for 
+    the labels for each category. This function will
+    plot the top five fuel consumption year
     """
-    df_actual_dataset = (dataset
-                         .groupby("YEAR")['FUEL CONSUMPTION']
-                         .sum().sort_values(ascending = False)
-                         .reset_index())
-    plt.pie(df_actual_dataset['FUEL CONSUMPTION'][:5]
-          , labels = df_actual_dataset['YEAR'][:5]
+    #set the size of the figure
+    plt.figure(figsize=(14,9))
+    # plot
+    plt.pie(x
+          , labels = y
           , autopct = "%1.2f%%")
+    # plot show
     plt.show()
     
-"""
-Function for the Bar plot graph
-"""
-def draw_bar_plot_graph(dataset):
+
+def draw_bar_plot_graph(x, y):
     """
     Bar plot graph 
     
-    for year and consumption
+    This function will accept 2 parameter one for the X
+    axis and the second for the Y axis to draw a plot.
+    X parameter contains the top five Make of the cars who consume 
+    the most of the fuel over the years and the second
+    Y parameter contains the fuel consumption values
+    for the y axis
     """
-    df_actual_dataset = (dataset
-                         .groupby("MAKE")['FUEL CONSUMPTION']
-                         .sum().sort_values(ascending = False)
-                         .reset_index())
+    #set the size of the figure
+    plt.figure(figsize=(14,9))
+    
     fig = plt.figure()
+    
+    #adding axes
     ax = fig.add_axes([0,0,1,1])
-    ax.bar(df_actual_dataset['MAKE'][:5]
-            ,df_actual_dataset['FUEL CONSUMPTION'][:5])
+    
+    # plot
+    ax.bar(x, y)
+    
+    #show plot
     plt.show()
     
-    
+
+# calling the function to read dataset
 df = read_dataSet('Fuel_Consumption_2000-2022.csv')
-draw_line_plot_graph(df)
-draw_pie_plot_graph(df)
-draw_bar_plot_graph(df)
+
+#Grouping the dataset at years and get sum of the Fuel consumption
+dataset_line_plot = (df
+                      .groupby("YEAR")['FUEL CONSUMPTION']
+                      .sum()
+                      .reset_index()
+                      )
+
+"""
+Grouping the dataset at years and fuel consumption but sorted 
+it in descending order used top five for pie plot
+"""
+dataset_pie_plot = (df
+                     .groupby("YEAR")['FUEL CONSUMPTION']
+                     .sum().sort_values(ascending = False)
+                     .reset_index())
+"""
+Grouping the datset at make and fuel consumption in descending
+order and pass top five values on the x and y axis to plot bar chat
+"""
+dataset_bar_plot = (df
+                    .groupby("MAKE")['FUEL CONSUMPTION']
+                    .sum().sort_values(ascending = False)
+                    .reset_index())
+# calling the function for line plot
+draw_line_plot_graph(dataset_line_plot['YEAR'], dataset_line_plot['FUEL CONSUMPTION'])
+
+# calling the function for pie plot
+draw_pie_plot_graph(dataset_pie_plot['FUEL CONSUMPTION'][:5], dataset_pie_plot['YEAR'][:5])
+
+# calling function for the bar plot
+draw_bar_plot_graph(dataset_bar_plot['MAKE'][:5], dataset_bar_plot['FUEL CONSUMPTION'][:5])
